88483328ce77b9024f1a584ddc5ce09c0f75dab2
Aaron C. Bell <aaroncnb@users.noreply.github.com>
2016-03-08 23:15:45 +0900

    Update q_all_2.sh

--- q_all_2.sh
+++ q_all_2.sh
@@ -64,3 +64,18 @@ q_list_stat NL_SLIT1_320x80_Wcm-2um-1.fits 1 - 16:30 1 | awk '{print $2*0.019889
 
 
 
+#####Now, since we have two slit positions for our current data-set, we re-run the above steps for the 2nd position
+##### There aren't an comments for this next part, since the steps in this script are identical for both positons
+##### Additionally, the lines where we dealt with the Standard Star data and calibration? We don't need to do that again.
+##### We use the same standard star data to calibrate both positions.
+##### POSITION B: Go!
+
+
+q_list_stat NL_SLIT2_interp_hotfix.fits 1 - - 1 | awk '{print $2,$3,$5}' > NL_SLIT2.TXT
+paste NL_SLIT2.TXT | awk '{print $3}' > NL_SLIT2.ADU
+q_mkimg NL_SLIT2.ADU NL_SLIT2_320x80.fits 320 80 > /dev/null
+q_arith NL_SLIT2_320x80.fits x STD_NL_FILTER_320x80_FL.fits NL_SLIT2_320x80_Wcm-2um-1.fits 
+q_arith NL_SLIT2_320x80.fits x STD_NL_FILTER_320x80_FN.fits NL_SLIT2_320x80_JY.fits 
+q_list_stat NL_SLIT2_320x80_Wcm-2um-1.fits 1 - 35:48 1 | awk '{print $2*0.0198899992+6.94999981,$5*14}' > SLIT2_Y35-48_Wcm-2um-1.SPC
+q_list_stat NL_SLIT2_320x80_Wcm-2um-1.fits 1 - 16:30 1 | awk '{print $2*0.0198899992+6.94999981,$5*15}' > SLIT2_Y16-30_Wcm-2um-1.SPC
+
